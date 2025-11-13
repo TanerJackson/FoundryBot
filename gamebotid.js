421ff91532f3ffc320c7b69bda677c3db296e3b4
TanerJackson <jacksontaner@gmail.com>
2025-11-12 22:12:51 -0600

    more testing

---
+++ gamebotid.js
@@ -0,0 +1,3 @@
+//THIS ID IS NOT CORRECT
+let foundrybotid = "113822458200588294";
+export default foundrybotid;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //alrighty boyo lets go
-const { Client, GatewayIntentBits, messageLink } = require("discord.js");
-require("dotenv").config();
+import foundrybotid from "./gamebotid.js";
+import dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+
+dotenv.config();
+const dropbotid = foundrybotid;
 
 const client = new Client({
   intents: [
@@ -14,6 +18,7 @@ client.once("ready", () => {
   console.log(`Bot ready to go, logged in as ${client.user.tag}`);
 });
 
+//EVERYTHING BELOW ARE TEST CASES
 client.on("messageCreate", (message) => {
   if (message.content === "!test1") {
     message.reply("test success");
@@ -39,7 +44,7 @@ client.on("messageCreate", async (message) => {
 
     console.log("Bold text found:", boldText);
 
-    // reply or react when bold text is found
+    //reply or react when bold text is found
     await message.react("✅");
     await message.reply(
       `Bold text used in your message: ${boldText.join(", ")}`
@@ -53,4 +58,30 @@ client.on("messageCreate", async (message) => {
     return;
   }
 });
+
+client.on("messageCreate", async (message) => {
+  if (message.content === "!usertest") {
+    console.log(message.user);
+    message.reply(`User: ${message.author.username}  Id: ${message.author.id}`);
+    return;
+  }
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.content === "thank you bot") {
+    message.reply(`You got it ${message.author.username}`);
+  }
+  return;
+});
 client.login(process.env.TOKEN);
+
+client.on("messageCreate", async (message) => {
+  if (message.content !== "!verify") return;
+  if (message.content === "!verify" && message.author.id === dropbotid) {
+    message.reply(`You are tha BOIIIIIII`);
+    return;
+  } else {
+    message.reply(`FALSE BOI DETECTED`);
+    return;
+  }
+});
