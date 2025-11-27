4f3ae541e0297c21de2886a3a85532d96219d975
TanerJackson <jacksontaner@gmail.com>
2025-11-27 17:17:46 -0600

    missed cleanup save in cpoints

--- commands/addcpoint.js
+++ commands/addcpoint.js
@@ -22,18 +22,15 @@ export default {
     const amount = interaction.options.getInteger("amount");
     const user = interaction.options.getUser("target");
 
-    // Admin check
     if (!interaction.client.isAdmin(interaction.user.id)) {
       return interaction.reply({ content: "Not authorized.", ephemeral: true });
     }
 
-    // Convert user to GuildMember
     const member = interaction.guild.members.cache.get(user.id);
     if (!member) {
       return interaction.reply({ content: "User not found.", ephemeral: true });
     }
 
-    // Build a fakeMessage compatible with old function
     const fakeMessage = {
       mentions: {
         members: new Map([[member.id, member]]),
